96dcde794edcfcadc1edae2a1b22c2ff82a046e3
Harsh Sihotra <harshsihotra@gmail.com>
2019-08-17 22:10:09 +0530

    Initial Commit

---
+++ src/main/java/com/fitness/edge/fitnessedge/FitnessEdgeApplication.java
@@ -0,0 +1,13 @@
+package com.fitness.edge.fitnessedge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessEdgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessEdgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitness/edge/fitnessedge/FitnessEdgeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fitness.edge.fitnessedge;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FitnessEdgeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
