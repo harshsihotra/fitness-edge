b099824fe7e2b416dca8232f3166715ebbfa17e7
Harsh Sihotra <harshsihotra@gmail.com>
2019-08-18 22:01:00 +0530

    Person Fix

--- src/main/java/com/fitness/edge/fitnessedge/model/Person.java
+++ src/main/java/com/fitness/edge/fitnessedge/model/Person.java
@@ -3,7 +3,6 @@ package com.fitness.edge.fitnessedge.model;
 import java.util.Date;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
@@ -24,7 +23,6 @@ public class Person {
 	@Id
 	@Field("_id")
 	private String id;
-	@Indexed(unique = true)
 	private String username;
 	private String firstName;
 	private String lastName;
--- src/main/java/com/fitness/edge/fitnessedge/repository/PersonRepository.java
+++ src/main/java/com/fitness/edge/fitnessedge/repository/PersonRepository.java
@@ -1,10 +1,15 @@
 package com.fitness.edge.fitnessedge.repository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.BasicQuery;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.CriteriaDefinition;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -29,9 +34,13 @@ public class PersonRepository {
 	
 	public List<Person> getPerson(Person person) throws Exception{
 		log.info("getting Person :"+ person);
-		BasicQuery basicQuery = new BasicQuery(new ObjectMapper().writeValueAsString(person));
-		log.info("query string :"+basicQuery.toString());
-		return mongoTemplate.find(basicQuery, Person.class);
+		HashMap<String,Object> hashMap = new ObjectMapper().convertValue(person, HashMap.class);
+		Query query = new Query();
+		for(Entry<String, Object> entry : hashMap.entrySet()) {
+			query.addCriteria(Criteria.where(entry.getKey()).is(entry.getValue()));
+		}
+		log.info("query string :"+query.toString());
+		return mongoTemplate.find(query, Person.class);
 	}
 
 }
